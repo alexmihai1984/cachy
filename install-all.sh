f518e23cec98f483dd66f181938c67afe2b45749
Alex Mihai <alexmihai1984@gmail.com>
2026-03-21 15:05:29 +0200

    docker and reorder

--- install-all.sh
+++ install-all.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 ./install/install-yay.sh
+
+./install/install-antigravity.sh
 ./install/install-chrome.sh
-./install/install-zen.sh
-./install/install-vscode.sh
 ./install/install-discord.sh
-./install/install-ncdu.sh
-./install/install-tmux.sh
-./install/install-printer.sh
+./install/install-docker.sh
 ./install/install-fatrace.sh
+./install/install-intellij.sh
 ./install/install-iotop.sh
 ./install/install-lsof.sh
-./install/install-intellij.sh
+./install/install-ncdu.sh
+./install/install-printer.sh
 ./install/install-sdkman.sh
-./install/install-antigravity.sh
+./install/install-tmux.sh
+./install/install-vscode.sh
+./install/install-zen.sh

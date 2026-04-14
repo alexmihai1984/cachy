8e4bd4b31ea1c47eb2c7e010781c3805c2f89739
Alex Mihai <alexmihai1984@gmail.com>
2026-04-14 20:35:34 +0300

    sublime

--- install-nice.sh
+++ install-nice.sh
@@ -3,6 +3,7 @@
 ./install/install-discord.sh
 ./install/install-jellyfin.sh
 ./install/install-neofetch.sh
+./install/install-sublime.sh
 ./install/install-tmux.sh
 ./install/install-ventoy.sh
 ./install/install-zen.sh

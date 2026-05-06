fe0612be770dbb48a3fa55fd9dc1e1ce7b4f748d
Alex Mihai <alexmihai1984@gmail.com>
2026-05-06 21:26:52 +0300

    kubectl + helm

--- install-nice.sh
+++ install-nice.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ./install/install-discord.sh
+./install/install-helm.sh
 ./install/install-jellyfin.sh
 ./install/install-k9s.sh
+./install/install-kubectl.sh
 ./install/install-neofetch.sh
 ./install/install-sublime.sh
 ./install/install-tmux.sh
